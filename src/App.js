e3d1fc23086015cc8f6febc471bdc781ba21411e
gowridurgakodela <gowridurgakodela@gmail.com>
2024-08-23 18:35:01 +0000

    skills

---
+++ src/App.js
@@ -0,0 +1,26 @@
+//import './App.css'
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages'
+import About from './pages/about'
+import Contact from './pages/contact'
+import SignUp from './pages/signup'
+import SignIn from './pages/signin'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
